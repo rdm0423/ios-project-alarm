601b9556f95d171b93ca7abd35a0192f0aa79e6d
Ross McIlwaine <rdm0423@gmail.com>
2016-05-16 15:05:37 -0600

    Adds Alarm Model and Model Controller

---
+++ Alarm/AlarmController.swift
@@ -0,0 +1,42 @@
+//
+//  AlarmController.swift
+//  Alarm
+//
+//  Created by Ross McIlwaine on 5/16/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import Foundation
+
+class AlarmController {
+    
+    static let sharedController = AlarmController()
+    
+    var alarms: [Alarm] = []
+    
+    
+    func addAlarm(fireTimeFromMidnight: NSTimeInterval, name: String) -> Alarm {
+        
+        let alarm = Alarm(fireTimeFromMidnight: fireTimeFromMidnight, name: name)
+        alarms.append(alarm)
+        
+        return alarm
+    }
+    
+    func updateAlarm(alarm: Alarm, fireTimeFromMidnight: NSTimeInterval, name: String) {
+        
+        alarm.fireTimeFromMidnight = fireTimeFromMidnight
+        alarm.name = name
+    }
+    
+    func deleteAlarm(alarm: Alarm) {
+        
+        guard let index = alarms.indexOf(alarm) else {
+            return
+        }
+        alarms.removeAtIndex(index)
+    }
+    
+    
+    
+}
